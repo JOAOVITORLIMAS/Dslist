900d9177645f1e1aebb111ba8ff7e9e60b507d40
JOAOVITORLIMAS <joaovitorlimas@gmail.com>
2023-05-08 21:22:47 -0300

    Project Created

---
+++ src/test/java/albuquerquelima/DSlist/DSlistApplicationTests.java
@@ -0,0 +1,13 @@
+package albuquerquelima.DSlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
